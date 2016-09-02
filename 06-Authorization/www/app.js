90fa7e4429decad00549a1b0d078c38a04804538
Valerii Radchenko <valerii.radchenko@akvelon.com>
2016-09-02 14:29:16 +0300

    06-Authorization Lock 10 sample updates

---
+++ 06-Authorization/www/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+(function () {
+
+  'use strict';
+
+  angular
+    .module('app', ['ionic', 'auth0.lock', 'angular-jwt'])
+    .config(config);
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', 'lockProvider', 'jwtOptionsProvider'];
+
+  function config($stateProvider, $urlRouterProvider, lockProvider, jwtOptionsProvider) {
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('home', {
+        url: '/',
+        templateUrl: 'components/home/home.html'
+      })
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'components/login/login.html'
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/');
+
+    lockProvider.init({
+      clientID: AUTH0_CLIENT_ID,
+      domain: AUTH0_DOMAIN,
+      options: {
+        auth: {
+          redirect: false,
+          params: {
+            scope: 'openid',
+            device: 'Mobile device'
+          }
+        }
+      }
+    });
+
+    // Configuration for angular-jwt
+    jwtOptionsProvider.config({
+      tokenGetter: function() {
+        return localStorage.getItem('id_token');
+      },
+      whiteListedDomains: ['localhost'],
+      unauthenticatedRedirectPath: '/login'
+    });
+
+  }
+
+})();
