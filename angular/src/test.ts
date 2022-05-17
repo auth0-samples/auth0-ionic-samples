5ae1fd04a6b111d3acdbf437d9da74a627961a70
Steve Hobbs <steve.hobbs@auth0.com>
2022-05-17 18:49:35 +0100

    chore: update remaining dependencies

--- angular/src/test.ts
+++ angular/src/test.ts
@@ -4,13 +4,17 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     <T>(id: string): T;
+    keys(): string[];
   };
 };
 
